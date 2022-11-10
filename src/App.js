3f2fc949dc6c9847f97409519324c25eb63a4592
Annie Tran <88398285+abt14@users.noreply.github.com>
2022-11-09 19:44:01 -0800

    created new branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
-import styled from 'styled-components';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import styled from "styled-components";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const [ data, setData ] = useState(null);
-  
+  const [data, setData] = useState(null);
+
   useEffect(() => {
-		fetch("<TODO: insert api url here>")
-		.then(res => res.json())
-		.then(res => setData(res.data['article.aml']))
-  }, [])
+    fetch("<TODO: insert api url here>")
+      .then((res) => res.json())
+      .then((res) => setData(res.data["article.aml"]));
+  }, []);
 
   return (
     <div className="App">
-      <Header/>
-      Hello Daily Bruin!
-      <Footer/>
+      <Header />
+      Branch for article grid
+      <Footer />
     </div>
   );
 }
