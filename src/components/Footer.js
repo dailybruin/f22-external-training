44a30ebc4211bfb8a8de4a898962af7e2f8e9a63
Yifan Tang <yifant@g.ucla.edu>
2022-11-30 19:27:29 -0800

    made footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,29 +9,39 @@ import { mediaQueries } from '../shared/config';
 
 
 const Container = styled.div`
-	background-color: black;
+	background-color: #EDE8E5;
 	height: auto;
-	color: white;
+	color: #434343;
 	padding: 60px 0 10px 0;
-    text-align: center;
+  text-align: center;
+  box-shadow: 0 4px 4px rgb(0,0,0,.25);
 	h1 {
 		margin: 0;
+    margin-bottom: 30px;
 		font-family: 'Times New Roman', Times, serif;
-    font-size: 30px;
+    font-size: 64px;
 	}
   /* margin-bottom: 20px; */
 `
+const Container2 = styled.div`
+	background-color: #D9D0C5;
+	height: auto;
+	padding: 64px 115px 59px 115px;
+  border-top: 7px dashed #706A61;
+`
 
 const Socials = styled.div`
-    position: relative;
+  position: relative;
 	margin-top: 20px;
   
 	img {
-
     position: relative;
 		margin: 0 20px;
+    width: 64px;
+    height: 64px;
 	}
 	p {
+    font-size: 24px;
     position: relative;
 		margin: 80px 20px;
 		${mediaQueries.mobile} {
@@ -42,17 +52,20 @@ const Socials = styled.div`
 
 const Footer = () => {
     return (
+      <Container2> 
         <Container>
-        <h1>DAILY BRUIN</h1>
-        <Socials>
-          <a href="https://www.instagram.com/dailybruin" target="_blank"><img src={insta}/></a>
-          <a href="https://www.facebook.com/dailybruin" target="_blank"><img src={fb}/></a>
-          <a href="https://www.twitter.com/dailybruin" target="_blank"><img src={twitter}/></a>
-          <a href="https://www.tiktok.com/@dailybruin" target="_blank"><img src={tiktok}/></a>
-          <a href="http://eepurl.com/cFEiZX" target="_blank"><img src={email}/></a>
-          <p>Built with Suzy’s ♥ in Kerckhoff 118 by [developers]. Designed by [designers].</p>
-        </Socials>
-    </Container>
+          <h1>DAILY BRUIN</h1>
+          <Socials>
+            <a href="https://www.instagram.com/dailybruin" target="_blank"><img src={insta}/></a>
+            <a href="https://www.facebook.com/dailybruin" target="_blank"><img src={fb}/></a>
+            <a href="https://www.twitter.com/dailybruin" target="_blank"><img src={twitter}/></a>
+            <a href="https://www.tiktok.com/@dailybruin" target="_blank"><img src={tiktok}/></a>
+            <a href="http://eepurl.com/cFEiZX" target="_blank"><img src={email}/></a>
+            <p>Built with Suzy’s ♥ in Kerckhoff 118 by [developers]. Designed by [designers].</p>
+          </Socials>
+        </Container>
+
+      </Container2>
     )
   }
   
